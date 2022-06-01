482f60aafaa96644fa8f6fa4f4bd1b5c6f9e3e29
judsonpaiva <judson.paiva@hotmail.com>
2022-06-01 23:06:51 +0100

    copiar classes para o project maven

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,18 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa(1, "Judson Paiva", "judsonpaiva16@gmail.com");
+		Pessoa p2 = new Pessoa(2, "Leonor Paiva", "leonajose2020@gmail.com");
+		Pessoa p3 = new Pessoa(3, "Jéssica Paiva", "jessicavanda@gmail.com");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		
+	}
+}
