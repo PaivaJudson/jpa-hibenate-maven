2e8cee3379cbabb2a677e0abe6cf5444e81d2195
judsonpaiva <judson.paiva@hotmail.com>
2022-06-02 00:25:32 +0100

    persistir objectos para o banco de dados

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,20 +1,30 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
+
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Judson Paiva", "judsonpaiva16@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Leonor Paiva", "leonajose2020@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Jéssica Paiva", "jessicavanda@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Judson Paiva", "judsonpaiva16@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Leonor Paiva", "leonajose2020@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Jéssica Paiva", "jessicavanda@gmail.com");
 		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 		
-		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		System.out.println("Início");
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Pronto!!");
 		
 	}
 }
